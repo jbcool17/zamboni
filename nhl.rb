5cb23d56c364e11a663d2d8efbcf3d1b1f1b63ae
John Brilla <jbrilla17@gmail.com>
2015-12-09 13:06:45 +1100

    started grabbing standings

--- nhl.rb
+++ nhl.rb
@@ -1,10 +1,11 @@
 require 'nokogiri'
 require 'open-uri'
 
-
+# Connet to 
 doc = Nokogiri::HTML(open("http://www.nhl.com/ice/standings.htm"));
+# doc = Nokogiri::HTML(open("http://www.nhl.com")); - doesn't want to work
 
-teamMenu = doc.css("#teamMenu")
+teamMenu = doc.css("div#teamMenu")
 
 teams = [];
 teamMenu.children.each do |team|
@@ -19,3 +20,8 @@ def get_team_homepage(list)
 end
 
 get_team_homepage(teams)
+
+
+#Getting Standings Data frin table
+doc.css('table.standings') # returns 4
+
